e4c7239920ea9d068f0232404f6749929cce573c
ilham <kubokabi@gmail.com>
2024-11-05 21:16:20 +0700

    update db

--- dbpenjualan_0030.sql
+++ dbpenjualan_0030.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 04, 2024 at 09:30 AM
+-- Generation Time: Nov 05, 2024 at 02:15 PM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- PHP Version: 8.2.17
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -39,9 +39,31 @@ CREATE TABLE `barang` (
 --
 
 INSERT INTO `barang` (`kode_barang`, `nama_barang`, `harga`, `stok`) VALUES
-('a001', 'Contoh Barang A', 150000, 100),
-('a002', 'Contoh Barang B', 250000, 50),
-('a003', 'Contoh Barang C', 100000, 200);
+('a001', 'Contoh Barang A', 150000, 86),
+('a002', 'Contoh Barang B', 250000, 43),
+('a003', 'Contoh Barang C', 100000, 198);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_transaksi`
+--
+
+CREATE TABLE `detail_transaksi` (
+  `id_detail` int(11) NOT NULL,
+  `id_transaksi` int(11) NOT NULL,
+  `kode_barang` varchar(10) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `detail_transaksi`
+--
+
+INSERT INTO `detail_transaksi` (`id_detail`, `id_transaksi`, `kode_barang`, `jumlah`, `harga`) VALUES
+(12, 6749, 'a003', 2, 100000),
+(13, 6749, 'a002', 4, 250000);
 
 -- --------------------------------------------------------
 
@@ -67,26 +89,24 @@ INSERT INTO `pelanggan` (`id_pelanggan`, `nama_pelanggan`, `alamat`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `transaksi_penjualan`
+-- Table structure for table `transaksi`
 --
 
-CREATE TABLE `transaksi_penjualan` (
+CREATE TABLE `transaksi` (
   `id_transaksi` int(11) NOT NULL,
-  `kode_barang` varchar(10) NOT NULL,
   `id_pelanggan` varchar(10) NOT NULL,
-  `jumlah` int(11) NOT NULL,
   `total_harga` int(11) NOT NULL,
-  `tanggal` datetime NOT NULL
+  `tanggal` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `transaksi_penjualan`
+-- Dumping data for table `transaksi`
 --
 
-INSERT INTO `transaksi_penjualan` (`id_transaksi`, `kode_barang`, `id_pelanggan`, `jumlah`, `total_harga`, `tanggal`) VALUES
-(1, 'a001', 'p001', 2, 300000, '2024-10-29 14:28:24'),
-(2, 'a002', 'p002', 1, 250000, '2024-10-29 14:28:24'),
-(3, 'a003', 'p003', 5, 500000, '2024-10-29 14:28:24');
+INSERT INTO `transaksi` (`id_transaksi`, `id_pelanggan`, `total_harga`, `tanggal`) VALUES
+(6746, 'p001', 700000, '2024-11-05 08:46:58'),
+(6748, 'p001', 0, '2024-11-05 08:53:42'),
+(6749, 'p001', 0, '2024-11-05 09:04:27');
 
 --
 -- Indexes for dumped tables
@@ -99,17 +119,24 @@ ALTER TABLE `barang`
   ADD PRIMARY KEY (`kode_barang`);
 
 --
+-- Indexes for table `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  ADD PRIMARY KEY (`id_detail`),
+  ADD KEY `id_transaksi` (`id_transaksi`),
+  ADD KEY `kode_barang` (`kode_barang`);
+
+--
 -- Indexes for table `pelanggan`
 --
 ALTER TABLE `pelanggan`
   ADD PRIMARY KEY (`id_pelanggan`);
 
 --
--- Indexes for table `transaksi_penjualan`
+-- Indexes for table `transaksi`
 --
-ALTER TABLE `transaksi_penjualan`
+ALTER TABLE `transaksi`
   ADD PRIMARY KEY (`id_transaksi`),
-  ADD KEY `kode_barang` (`kode_barang`),
   ADD KEY `id_pelanggan` (`id_pelanggan`);
 
 --
@@ -117,21 +144,33 @@ ALTER TABLE `transaksi_penjualan`
 --
 
 --
--- AUTO_INCREMENT for table `transaksi_penjualan`
+-- AUTO_INCREMENT for table `detail_transaksi`
 --
-ALTER TABLE `transaksi_penjualan`
-  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `detail_transaksi`
+  MODIFY `id_detail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6750;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `transaksi_penjualan`
+-- Constraints for table `detail_transaksi`
 --
-ALTER TABLE `transaksi_penjualan`
-  ADD CONSTRAINT `transaksi_penjualan_ibfk_1` FOREIGN KEY (`kode_barang`) REFERENCES `barang` (`kode_barang`),
-  ADD CONSTRAINT `transaksi_penjualan_ibfk_2` FOREIGN KEY (`id_pelanggan`) REFERENCES `pelanggan` (`id_pelanggan`);
+ALTER TABLE `detail_transaksi`
+  ADD CONSTRAINT `detail_transaksi_ibfk_1` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`) ON DELETE CASCADE,
+  ADD CONSTRAINT `detail_transaksi_ibfk_2` FOREIGN KEY (`kode_barang`) REFERENCES `barang` (`kode_barang`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`id_pelanggan`) REFERENCES `pelanggan` (`id_pelanggan`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
